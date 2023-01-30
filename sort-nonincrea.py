be3782558927081ee7f7b8d5fc888397cc811da0
Kilvia <kilvialeticia@hotmail.com>
2023-01-30 14:54:31 +0900

    Implement sort non increasing order in python

---
+++ sort-nonincrea.py
@@ -0,0 +1,15 @@
+def sort(seq):
+    for i in range(1, len(seq), 1):
+        key = seq[i]
+        j = i - 1
+        while j >= 0 and seq[j] < key:
+            seq[j+1] = seq[j]
+            j -= 1
+        seq[j+1] = key
+            
+
+
+if __name__ == '__main__':
+    cards = [10, 6, 1, 8, 2, 4]
+    sort(cards)
+    print(cards)
