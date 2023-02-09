7ee597f947940dfca3799f67405877e167881132
Kilvia <kilvialeticia@hotmail.com>
2023-02-09 11:42:03 +0900

    Change directory

---
+++ sort/insert-sort.py
@@ -0,0 +1,18 @@
+# Input: a sequence of n numbers
+# Output: a reordering of the input sequences in a non decreasing order
+
+def sort(seq):
+    for i in range(1, len(seq), 1):
+        key = seq[i]
+        j = i - 1
+        while j >= 0 and seq[j] > key:
+            seq[j+1] = seq[j]
+            j -= 1
+        seq[j+1] = key
+            
+
+
+if __name__ == '__main__':
+    cards = [10, 6, 1, 8, 2, 4]
+    sort(cards)
+    print(cards)
