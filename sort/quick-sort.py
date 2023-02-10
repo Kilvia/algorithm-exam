dc2480075b246ca1ab2c1c29b75670553cac4ad0
Kilvia <kilvialeticia@hotmail.com>
2023-02-10 15:34:07 +0900

    Separe quicksort into diferent functions

--- sort/quick-sort.py
+++ sort/quick-sort.py
@@ -35,7 +35,7 @@ def partition_mod(A, p, r):
     else:
         return (i+2)
 
-def random_partition(A, p, r):
+def partition_random(A, p, r):
     i = random.randint(p-1, r-1) 
     A[r-1], A[i] = A[i], A[r-1]   
 
@@ -43,9 +43,25 @@ def random_partition(A, p, r):
 
 def quicksort(A, p, r):
     if p < r:
-        # # q = partition(A, p, r)
-        # q = partition_mod(A, p, r)
-        q = random_partition(A, p, r)
+        q = partition(A, p, r)
+
+        # since A[q] is already in the right position call quick sort again for it left and right side elements
+        quicksort(A, p, q-1)
+        quicksort(A, q+1, r)
+
+# Quick sort modified for arrays with all elements equals
+def quicksort_mod(A, p, r):
+    if p < r:
+        q = partition_mod(A, p, r)
+
+        # since A[q] is already in the right position call quick sort again for it left and right side elements
+        quicksort(A, p, q-1)
+        quicksort(A, q+1, r)
+
+# Quick sort selecting a random pivot element
+def quicksort_random(A, p, r):
+    if p < r:
+        q = partition_random(A, p, r)
 
         # since A[q] is already in the right position call quick sort again for it left and right side elements
         quicksort(A, p, q-1)
@@ -54,6 +70,12 @@ def quicksort(A, p, r):
 if __name__ == '__main__':
     # A = [2,8,7,1,3,5,6,4]
     A = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11]
-    # A = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     quicksort(A, 1, 12)
+    print(A)
+
+    quicksort_random(A, 1, 12)
+    print(A)
+
+    A = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    quicksort_mod(A, 1, 12)
     print(A)
